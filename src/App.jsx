3cfac12a4e200d663cd60ccf80607cdc3feadedb
Cory Pollard <astandard87@gmail.com>
2023-01-18 13:14:53 -0600

    Added cart to render programatically

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 const App = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const modalHandler = () => {
+    setModalOpen(open => (open = !open));
+  };
+
   return (
     <>
-      <Header />
+      {modalOpen && <Cart onOpenCart={modalHandler} />}
+      <Header onOpenCart={modalHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,12 +3,12 @@ import classes from './Header.module.css';
 import headerImg from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = props => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onOpenCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={headerImg} alt="Table with a variety of delicious foods" />
