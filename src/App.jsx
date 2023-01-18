2e97b71e4e6143176755caa4dc13c36d50e2e623
Cory Pollard <astandard87@gmail.com>
2023-01-18 10:37:01 -0600

    Added header and cart button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Layout/Header';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+};
 
-export default App
+export default App;
